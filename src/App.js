878911158a519a41700e83c3dfa3c0f3012ff6f6
luozyiii <924361501@qq.com>
2022-01-13 01:29:24 +0800

    使用上下文读写state

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {useState, useContext} from 'react';
+import './App.css';
+
+const appContext = React.createContext(null);
+const App = () => {
+  const [appState, setAppState] = useState({
+    user: {
+      name: '小明',
+      age: 18
+    }
+  })
+  const contextValue = {appState, setAppState}
+  return (
+    <appContext.Provider value={contextValue}>
+      <A/>
+      <B/>
+      <C/>
+    </appContext.Provider>
+  )
+}
+
+const A = () => <section>大儿子<User/></section>;
+const B = () => <section>二儿子<UserModifier/></section>;
+const C = () => <section>三儿子</section>;
+
+const User = () => {
+  const contextValue = useContext(appContext);
+  return <div>User: {contextValue.appState.user.name}</div>
+};
+const UserModifier = () => {
+  const contextValue = useContext(appContext);
+  const onChange = (e) => {
+    contextValue.appState.user.name = e.target.value;
+    contextValue.setAppState({...contextValue.appState})
+  }
+
+  return <div>
+    <input value={contextValue.appState.user.name} onChange={onChange}/>
+  </div>
+};
+
+export default App
